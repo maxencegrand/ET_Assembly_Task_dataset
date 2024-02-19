a60d7ebf678f69d6f80e0715f473b8b6b32d3e6f
Maxence Grand <Maxence.Grand@univ-grenoble-alpes.fr>
2024-02-19 13:26:33 +0100

    fix 3958

--- script/data_extraction.py
+++ script/data_extraction.py
@@ -38,9 +38,12 @@ def main(argv):
                 print("Extract %s" % figure)
                 stationary_extractor.Extractor(user, figure)
                 TableState(user, figure)
+                GoalState(user, figure)
                 Dataset(user,figure)
             except:
                 print("Error during extraction")
+                # traceback.print_exc()
+                # sys.exit(1)
 
 if __name__ == "__main__":
    main(sys.argv[1:])
